fd78a4a30e32412e6cab9d6fb816b0d8739a8e60
Daiana L <daianalimaep@gmail.com>
2025-07-19 20:44:38 -0300

    chore: remove Healthcheck

--- src/app.js
+++ src/app.js
@@ -4,5 +4,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('welcome!'));
-
+  
 app.listen(PORT, () => console.log(`☁️ Server rodando na porta ${PORT}`));
